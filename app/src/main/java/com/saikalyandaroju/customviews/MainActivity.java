77c65cceb2a23128acbb8dfb9a0adb19f38faeec
kalyan <darojusaikalyan.civ18@itbhu.ac.in>
2021-07-11 21:07:23 +0530

    animations_commit

--- app/src/main/java/com/saikalyandaroju/customviews/MainActivity.java
+++ app/src/main/java/com/saikalyandaroju/customviews/MainActivity.java
@@ -4,21 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.saikalyandaroju.customviews.views.CustomImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-    CustomImageView customView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       customView=findViewById(R.id.customview);
-       customView.setText("Custom Text");
-       customView.setImageResource(R.drawable.poster);
-       customView.setSize(200,200);
 
 
     }
