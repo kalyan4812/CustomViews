e77c5bc406a7b13c1d8672cefb1c9594297c463f
kalyan <darojusaikalyan.civ18@itbhu.ac.in>
2021-07-11 13:43:02 +0530

    gradient_commit

--- app/src/main/java/com/saikalyandaroju/customviews/MainActivity.java
+++ app/src/main/java/com/saikalyandaroju/customviews/MainActivity.java
@@ -1,20 +1,26 @@
 package com.saikalyandaroju.customviews;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout constraintLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        constraintLayout=findViewById(R.id.layout);
+        AnimationDrawable animationDrawable=(AnimationDrawable)constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
     }
 
-    public void gradientanim(View view) {
-        startActivity(new Intent(getApplicationContext(),GradientActivity.class));
-    }
+
 }
