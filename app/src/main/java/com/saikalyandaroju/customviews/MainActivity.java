135665094fce1b78dfa5d32d0eaf6bbe05eae267
kalyan <darojusaikalyan.civ18@itbhu.ac.in>
2021-07-11 13:52:25 +0530

    added_spannable_string_commit

--- app/src/main/java/com/saikalyandaroju/customviews/MainActivity.java
+++ app/src/main/java/com/saikalyandaroju/customviews/MainActivity.java
@@ -11,16 +11,40 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout constraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        constraintLayout=findViewById(R.id.layout);
-        AnimationDrawable animationDrawable=(AnimationDrawable)constraintLayout.getBackground();
+        constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
         animationDrawable.setEnterFadeDuration(2000);
         animationDrawable.setExitFadeDuration(4000);
         animationDrawable.start();
     }
 
+    /*
+     spannabel strings --> you can set part of text in one color or add backgorund to it.
+     other way is create text in strings.xml with color.
+
+     TextView textView = findViewById(R.id.text_view);
+
+        String text = "I want THIS and THIS to be colored";
+
+        SpannableString ss = new SpannableString(text); // immutable.
+        SpannableStringBuilder ssb = new SpannableStringBuilder(text); // mutable.
+
+        ForegroundColorSpan fcsRed = new ForegroundColorSpan(Color.RED);
+        ForegroundColorSpan fcsGreen = new ForegroundColorSpan(Color.GREEN);
+        BackgroundColorSpan bcsYellow = new BackgroundColorSpan(Color.YELLOW);
+
+        ssb.setSpan(fcsRed, 7, 11, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(fcsGreen, 16, 20, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        ssb.setSpan(bcsYellow, 27, 34, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        ssb.append(" and this to be appended");
+
+        textView.setText(ssb);
+     */
 
 }
